508daac20fd841df1d657d7706cd9956eebc317d
crossovka <cevyacheslav@gmail.com>
2022-11-24 00:59:03 +0900

    фикс скролла

--- scripts/burger__menu.js
+++ scripts/burger__menu.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
 	$('.header__burger').click(function (event) {
 		//при клике на бургер действия.нажал-добавился класс.нажали-убрался
 		$('.header__burger, .header__menu').toggleClass('active');
-		$('body').toggleClass('no-scroll');
+		$('body').toggleClass('lock');
 	});
 });
