8aa2bbd23f66efb63169b87b6bb9a91e117d4c08
crossovka <cevyacheslav@gmail.com>
2022-11-24 00:49:55 +0900

    фикс багов

--- scripts/burger__menu.js
+++ scripts/burger__menu.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
 	$('.header__burger').click(function (event) {
 		//при клике на бургер действия.нажал-добавился класс.нажали-убрался
 		$('.header__burger, .header__menu').toggleClass('active');
-		$('body').addClass('no-scroll');
+		$('body').toggleClass('no-scroll');
 	});
 });
