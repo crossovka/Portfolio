a3faf3062978e34b026fd74ac10f08631d5b7e0a
crossovka <cevyacheslav@gmail.com>
2022-11-24 17:13:41 +0900

    uncomented my code

--- scripts/burger__menu.js
+++ scripts/burger__menu.js
@@ -1,7 +1,7 @@
-// $(document).ready(function () {
-// 	$('.header__burger').click(function (event) {
-// 		//при клике на бургер действия.нажал-добавился класс.нажали-убрался
-// 		$('.header__burger, .header__menu').toggleClass('active');
-// 		$('body').toggleClass('lock');
-// 	});
-// });
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		//при клике на бургер действия.нажал-добавился класс.нажали-убрался
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
