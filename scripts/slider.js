20783c6488dcbb5a58752df27db81ee2b0caea49
crossovka <cevyacheslav@gmail.com>
2022-11-26 19:43:09 +0900

    финальные штрихи

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,21 +2,24 @@
 $(document).ready(function () {
 	// инициализация слайдера
 	$('.slider__content').slick({
+		adaptiveHeight: true,
 		// если слайды заканчиваются то все начинается заново
 		infinity: true,
 		// сколкьо показать
 		slidesToShow: 3,
 		// по сколько перелистывается
-		slidesToScroll: 3,
+		slidesToScroll: 2,
 		// скорость перелистывания при нажатии на точку
 		speed: 2500,
+		pauseOnHover: true,
 		// автоплей
 		autoplay: true,
-		autoplaySpeed: 1000,
+		autoplaySpeed: 1700,
 		// автоплей
 		// точки
 		dots: true,
 		arrows: false,
+		// waitForAnimate: false,
 		// точки
 		// адаптивность по брейкпоинтам из офф документации. брейкпоитны из containers.scss
 		responsive: [
@@ -26,6 +29,7 @@ $(document).ready(function () {
 				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 2,
+					easing: true,
 				},
 			},
 			{
@@ -34,15 +38,20 @@ $(document).ready(function () {
 				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 2,
+					speed: 900,
+					autoplaySpeed: 1800,
+					easing: true,
 				},
 			},
 			{
 				// мобилки
 				breakpoint: 767.98,
 				settings: {
-					dots: false,
 					slidesToShow: 1,
 					slidesToScroll: 1,
+					speed: 900,
+					autoplaySpeed: 1800,
+					easing: true,
 				},
 			},
 			{
@@ -52,6 +61,9 @@ $(document).ready(function () {
 					dots: false,
 					slidesToShow: 1,
 					slidesToScroll: 1,
+					speed: 900,
+					autoplaySpeed: 1800,
+					easing: true,
 				},
 			},
 			// You can unslick at a given breakpoint now by adding:
@@ -59,62 +71,8 @@ $(document).ready(function () {
 			// instead of a settings object
 		],
 	});
-	$('.slider__content-next').slick({
-		//  стартовый слайд
-		initialSlide: 4,
-		// если слайды заканчиваются то все начинается заново
-		infinity: true,
-		// сколкьо показать
-		slidesToShow: 2,
-		// по сколько перелистывается
-		slidesToScroll: 2,
-		// скорость перелистывания при нажатии на точку
-		speed: 2500,
-		// автоплей
-		autoplay: true,
-		autoplaySpeed: 2000,
-		// автоплей
-		// точки
-		dots: true,
-		arrows: false,
-		// точки
-		// адаптивность по брейкпоинтам из офф документации. брейкпоитны из containers.scss
-		responsive: [
-			// {
-			// 	//
-			// 	breakpoint:  1306,
-			// 	settings: {
-			// 		slidesToShow: 2,
-			// 		slidesToScroll: 2,
-			// 	}
-			// },
-			// {
-			// 	// планшеты
-			// 	breakpoint: 991.98,
-			// 	settings: {
-			// 		slidesToShow: 2,
-			// 		slidesToScroll: 2,
-			// 	}
-			// },
-			{
-				// мобилки
-				breakpoint: 767.98,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-				},
-			},
-			{
-				// мобилки поменьше
-				breakpoint: 565.98,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-				},
-			},
-			// You can unslick at a given breakpoint now by adding:
-			// settings: "unslick"
-			// instead of a settings object
-		],
+	$('.slider').on('afterChange', function (event, slick, currentSlide) {
+		console.log(currentSlide);
 	});
+	$('.slider').slick('setPosition');
 });
