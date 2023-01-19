c71501ae7a2aeff00b35c69b56eae42f92a94cbc
crossovka <cevyacheslav@gmail.com>
2023-01-19 23:32:29 +0900

    test mailer

--- scripts/sendmail.php
+++ scripts/sendmail.php
@@ -8,7 +8,7 @@ require '.libs/PHPmailer/Exception.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 $text = $_POST['text'];
-$file = $_FILES['myfile'];
+// $file = $_FILES['myfile'];
 
 // Формирование самого письма
 $title = "Заголовок письма";
@@ -22,37 +22,37 @@ $body = "
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
-    $mail->isSMTP();   
+    $mail->isSMTP();
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
     //$mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Host       = 'ssl://smtp.yandex.ru'; // SMTP сервера вашей почты
     $mail->Username   = 'Tchekasin314grupa@yandex.ru'; // Логин на почте
     $mail->Password   = 'Best_password314'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('mail@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+    $mail->setFrom('Tchekasin314grupa@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('youremail@yandex.ru');  
+    $mail->addAddress('vyacheslav.tchekasin@yandex.ru');  
     $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
 
     // Прикрипление файлов к письму
-if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
-        } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
-        }
-    }   
-}
+// if (!empty($file['name'][0])) {
+//     for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+//         $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+//         $filename = $file['name'][$ct];
+//         if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+//             $mail->addAttachment($uploadfile, $filename);
+//             $rfile[] = "Файл $filename прикреплён";
+//         } else {
+//             $rfile[] = "Не удалось прикрепить файл $filename";
+//         }
+//     }   
+// }
 // Отправка сообщения
 $mail->isHTML(true);
 $mail->Subject = $title;
@@ -69,4 +69,4 @@ else {$result = "error";}
 
 // Отображение результата
 echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
-view rawsend.php hosted with ❤ by GitHub
+// view rawsend.php hosted with ❤ by GitHub
